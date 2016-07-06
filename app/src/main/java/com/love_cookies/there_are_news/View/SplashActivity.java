0df23beddf4233911b177088fa85523583b2d5a9
Cookie <tsubasa-kun@foxmail.com>
2016-07-06 10:35:31 +0800

    初步的两个页面

--- app/src/main/java/com/love_cookies/there_are_news/View/SplashActivity.java
+++ app/src/main/java/com/love_cookies/there_are_news/View/SplashActivity.java
@@ -1,12 +1,12 @@
 package com.love_cookies.there_are_news.View;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.KeyEvent;
 import android.view.View;
 
 import com.love_cookies.cookie_library.Activity.BaseActivity;
-import com.love_cookies.cookie_library.Application.ActivityCollections;
-import com.love_cookies.cookie_library.Utils.ToastUtils;
 import com.love_cookies.there_are_news.R;
 
 import org.xutils.view.annotation.ContentView;
@@ -14,12 +14,21 @@ import org.xutils.view.annotation.ContentView;
 /**
  * Created by xiekun on 2016/7/6 0006.
  *
- * App主页
+ * App启动页
  */
-@ContentView(R.layout.activity_main)
-public class MainActivity extends BaseActivity {
+@ContentView(R.layout.activity_splash)
+public class SplashActivity extends BaseActivity {
 
-    private long exitTime;
+    private final int SPLASH_DISPLAY_DURATION = 1500;//启动页显示时长
+    private Looper looper = Looper.myLooper();
+    private Handler handler = new Handler(looper);
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            turnThenFinish(MainActivity.class);
+        }
+    };
 
     /**
      * 初始化控件
@@ -27,6 +36,7 @@ public class MainActivity extends BaseActivity {
      */
     @Override
     public void initWidget(Bundle savedInstanceState) {
+        handler.postDelayed(runnable, SPLASH_DISPLAY_DURATION);
     }
 
     /**
@@ -39,23 +49,15 @@ public class MainActivity extends BaseActivity {
     }
 
     /**
-     * 点两次返回退出程序
+     * 重写物理按键
      * @param keyCode
      * @param event
      * @return
      */
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
-            if ((System.currentTimeMillis() - exitTime) > 2000)
-            {
-                ToastUtils.show(getApplicationContext(), R.string.exit_tip);
-                exitTime = System.currentTimeMillis();
-            } else {
-                ActivityCollections.getInstance().finishAllActivity();
-                System.exit(0);
-            }
-            return true;
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            handler.removeCallbacks(runnable);
         }
         return super.onKeyDown(keyCode, event);
     }
